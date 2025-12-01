44a97bc60bfdb5297d245b339c2bf8f322784f2a
Jan Hrůza <honza.hruza.d5@hotmail.com>
2025-12-01 20:27:20 +0100

    Add project files.

---
+++ AoC2025/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace AoC2025;
+
+/// <summary>
+/// Representing the main program for Advent of Code 2025.
+/// </summary>
+internal class Program
+{
+    /// <summary>
+    /// Serves as the entry point for the application.
+    /// </summary>
+    /// <param name="args">An array of command-line arguments supplied to the application. Each element represents a single argument as a
+    /// string.</param>
+    /// <returns>An integer value that is returned to the operating system upon application exit. Typically, a return value of 0
+    /// indicates successful execution.</returns>
+    static int Main(string[] args)
+    {
+        return 0;
+    }
+}
