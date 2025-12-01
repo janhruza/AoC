39eaecdf306fdab905c7874f004d71435fac94ab
Jan Hrůza <honza.hruza.d5@hotmail.com>
2025-12-01 20:39:37 +0100

    Added project configuration.

---
+++ AoC2025/Puzzles/Puzzle1.cs
@@ -0,0 +1,30 @@
+﻿namespace AoC2025.Puzzles;
+
+/// <summary>
+/// Provides solutions for Day 1 of the puzzle challenge by implementing the required parts as defined by the <see
+/// cref="IDayPuzzle"/> interface.
+/// </summary>
+/// <remarks>Use this class to execute Part 1 and Part 2 solutions for Day 1 by supplying the puzzle input as a
+/// string. Each part returns an integer result based on the provided input.</remarks>
+public class Puzzle1 : IDayPuzzle
+{
+    /// <summary>
+    /// Processes the specified input string and returns the result for Part 1 of the problem.
+    /// </summary>
+    /// <param name="input">The input data to be evaluated. Cannot be null.</param>
+    /// <returns>An integer representing the computed result for Part 1. Returns -1 if the input does not produce a valid result.</returns>
+    public int Part1(string input)
+    {
+        return -1;
+    }
+
+    /// <summary>
+    /// Processes the specified input string and returns the result for Part 2 of the challenge.
+    /// </summary>
+    /// <param name="input">The input data to be evaluated for Part 2. Cannot be null.</param>
+    /// <returns>An integer representing the computed result for Part 2. Returns -1 if the input does not produce a valid result.</returns>
+    public int Part2(string input)
+    {
+        return -1;
+    }
+}
